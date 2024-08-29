b7a1486dfa662d92dc85869c8b69a84343717ddb
Chris Blackburn <cblackburn@launchdarkly.com>
2024-08-29 01:26:53 -0400

    carbonicus palette updates

--- lua/blaqbern/palettes/carbonicus.lua
+++ lua/blaqbern/palettes/carbonicus.lua
@@ -1,10 +1,10 @@
 return {
 	black = { normal = "#2b3236", bright = "#474747" },
 	white = { normal = "#dde1e6", bright = "#cdd3da" },
-	red = { normal = "#d02670", bright = "#ee5396" },
-	green = { normal = "#42be65", bright = "#6fdc8c" },
+	red = { normal = "#ff8389", bright = "#ffb3b8" },
+	green = { normal = "#6fdc8c", bright = "#a7f0ba" },
 	blue = { normal = "#059fff", bright = "#57b3ff" },
 	cyan = { normal = "#08bdba", bright = "#3ddbd9" },
 	magenta = { normal = "#be95ff", bright = "#d4bbff" },
-	yellow = { normal = "#ff8389", bright = "#ffb3b8" },
+	yellow = { normal = "#ff7eb6", bright = "#ffafd2" },
 }
