56058be39cfa264b59b159862bc66fea1436a76e
Chris Blackburn <cblackburn@launchdarkly.com>
2024-09-12 13:12:56 -0400

    allow config of alert colors

--- lua/blaqbern/palettes/carbonicus.lua
+++ lua/blaqbern/palettes/carbonicus.lua
@@ -1,10 +1,15 @@
 return {
-	black = { normal = "#2b3236", bright = "#474747" },
-	white = { normal = "#dde1e6", bright = "#cdd3da" },
-	red = { normal = "#ff8389", bright = "#ffb3b8" },
-	green = { normal = "#6fdc8c", bright = "#a7f0ba" },
-	blue = { normal = "#059fff", bright = "#57b3ff" },
-	cyan = { normal = "#08bdba", bright = "#3ddbd9" },
-	magenta = { normal = "#be95ff", bright = "#d4bbff" },
-	yellow = { normal = "#ff7eb6", bright = "#ffafd2" },
+  black = { normal = "#21272a", bright = "#2b3236" },
+  white = { normal = "#dde1e6", bright = "#cdd3da" },
+  red = { normal = "#ff8389", bright = "#ffb3b8" },
+  green = { normal = "#6fdc8c", bright = "#a7f0ba" },
+  blue = { normal = "#059fff", bright = "#57b3ff" },
+  cyan = { normal = "#08bdba", bright = "#3ddbd9" },
+  magenta = { normal = "#be95ff", bright = "#d4bbff" },
+  yellow = { normal = "#ff7eb6", bright = "#ffafd2" },
+
+  alert = "#fa4d56",
+  success = "#24a148",
+  warning = "#d2a106",
+  info = "#78a9ff",
 }
